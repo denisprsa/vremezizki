34caf00e6cb285f29dc61db8b4530766b55863ae
denisp <denis.prsa@inova.si>
2020-11-08 13:12:16 +0100

    Changed get theme function to return dark or light

--- frontend/src/features/theme/get-theme.ts
+++ frontend/src/features/theme/get-theme.ts
@@ -1,16 +1,32 @@
 import { createMuiTheme, Theme } from '@material-ui/core';
 import { blue, grey } from '@material-ui/core/colors';
 
+declare module '@material-ui/core/styles/createMuiTheme' {
+    interface Theme {
+        borders: {
+            containers: React.CSSProperties['color'],
+        }
+    }
+    interface ThemeOptions {
+        borders: {
+            containers: React.CSSProperties['color']
+        }
+    }
+  }
+
 export function getTheme(isDark: boolean): Theme {
     if (isDark) {
         return createMuiTheme({
+            borders: {
+                containers: 'transparent'
+            },
             typography: {
                 fontFamily: '"Open Sans", sans-serif'
             },
             palette: {
                 type: 'dark',
                 primary: {
-                    main: blue[900]
+                    main: blue[500]
                 },
                 secondary: {
                     main: grey[300]
@@ -18,8 +34,11 @@ export function getTheme(isDark: boolean): Theme {
             },
             
         });
-    } 
+    }
     return createMuiTheme({
+        borders: {
+            containers: '#c6d2e1'
+        },
         typography: {
             fontFamily: '"Open Sans", sans-serif'
         },
