8a2ea43e0970217e3e7cf610bd87bcc9001cff3b
denisp <denis.prsa@inova.si>
2020-11-08 13:13:00 +0100

    Added graph for current conditions

--- frontend/src/components/weather-station/current-conditions/current-conditions.tsx
+++ frontend/src/components/weather-station/current-conditions/current-conditions.tsx
@@ -1,12 +1,18 @@
 import React, { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from 'StoreTypes';
 import Box from '@material-ui/core/Box';
+import Carousel from '../../carousel/carousel';
+import CircleFillChart from '../../graphs/circle-fill/circle-filling';
+import CirclePointerChart from '../../graphs/circle-pointer/circle-pointer';
+import CircleWindChart from '../../graphs/circle-wind/circle-wind';
+import PrecipitationGraph from '../../graphs/precipitation/precipitation';
+import LoaderOverlay from '../../loader-overlay/loader-overlay';
+import MinMaxCurrentValues from '../min-max-current-values/min-max-current-values';
 
 import './current-conditions.scss';
 
 import { useContainerStyles } from '../../../layouts/custom-styles';
-import Carousel from '../../carousel/carousel';
-import CircleFillChart from '../../graphs/circle-fill/circle-filling';
-import CircleGaugeChart from '../../graphs/circle-gauge/circle-gauge';
 
 type Props = {
 
@@ -14,36 +20,121 @@ type Props = {
 
 const CurrentConditions: FunctionComponent<Props> = () => {
     const classes = useContainerStyles();
+    const { isLoadingWeatherData }  = useSelector((state: RootState) => ({
+        isLoadingWeatherData: state.weatherStation.isLoadingWeatherData,
+        weatherDataStatistic: state.weatherStation.weatherDataStatistic
+    }));
+
+    const widthAndHeight = 195;
 
     return (
         <>
             <Box mt={2}>
                 <Carousel marginLeftMobile={10}>
-                    <div className={`current-conditions-items ${classes.root}`}>
+                    <div className={`current-conditions-items ${classes.root} ${classes.currentConditionsBorder}`}>
                         <div className="current-conditions-items-title">
                             <h2>
                                 Temperatura
                             </h2>
                         </div>
-                        <CircleFillChart unit={'°C'} minValue={2.4} value={15.1} maxValue={22.3}/>
+                        <LoaderOverlay loading={isLoadingWeatherData} width={widthAndHeight} height={widthAndHeight}>
+                            <CircleFillChart unit={'°C'}
+                                minValue={2.4}
+                                value={15.1}
+                                maxValue={22.3}
+                                widthAndHeight={widthAndHeight}/>
+                        </LoaderOverlay>
+                        <MinMaxCurrentValues loading={isLoadingWeatherData}
+                            max={22.3}
+                            maxIcon="/assets/current-conditions/thermometer-max.svg"
+                            min={2.4}
+                            minIcon="/assets/current-conditions/thermometer-min.svg"
+                            unit="°C"/>
                     </div>
 
-                    <div className={`current-conditions-items ${classes.root}`}>
+                    <div className={`current-conditions-items ${classes.root} ${classes.currentConditionsBorder}`}>
                         <div className="current-conditions-items-title">
                             <h2>
                                 Rosišče
                             </h2>
                         </div>
-                        <CircleFillChart unit={'°C'} minValue={2.4} value={9.4} maxValue={22.3}/>
+                        <LoaderOverlay loading={isLoadingWeatherData} width={widthAndHeight} height={widthAndHeight}>
+                            <CircleFillChart unit={'°C'} minValue={-2.4} value={9.4} maxValue={19.3} widthAndHeight={widthAndHeight}/>
+                        </LoaderOverlay>
+                        <MinMaxCurrentValues loading={isLoadingWeatherData}
+                            max={19.3}
+                            maxIcon="/assets/current-conditions/thermometer-max.svg"
+                            min={-2.4}
+                            minIcon="/assets/current-conditions/thermometer-min.svg"
+                            unit="°C"/>
                     </div>
 
-                    <div className={`current-conditions-items ${classes.root}`}>
+                    <div className={`current-conditions-items ${classes.root} ${classes.currentConditionsBorder}`}>
                         <div className="current-conditions-items-title">
                             <h2>
                                 Vlaga
                             </h2>
                         </div>
-                        <CircleGaugeChart unit={'%'} minValue={34} value={65} maxValue={92}/>
+                        <LoaderOverlay loading={isLoadingWeatherData} width={widthAndHeight} height={widthAndHeight}>
+                            <CirclePointerChart unit={'%'} minValue={34} value={65} maxValue={92} widthAndHeight={195}/>
+                        </LoaderOverlay>
+                        <MinMaxCurrentValues loading={isLoadingWeatherData}
+                            max={92}
+                            maxIcon="/assets/current-conditions/pressure-gauge-max.svg"
+                            min={34}
+                            minIcon="/assets/current-conditions/pressure-gauge-min.svg"
+                            unit="%"/>
+                    </div>
+
+                    <div className={`current-conditions-items ${classes.root} ${classes.currentConditionsBorder}`}>
+                        <div className="current-conditions-items-title">
+                            <h2>
+                                Veter
+                            </h2>
+                        </div>
+                        <LoaderOverlay loading={isLoadingWeatherData} width={widthAndHeight} height={widthAndHeight}>
+                            <CircleWindChart unit={'km/h'} value={12.4} windDirection={200} widthAndHeight={195}/>
+                        </LoaderOverlay>
+                        <MinMaxCurrentValues loading={isLoadingWeatherData}
+                            max={45.6}
+                            maxIcon="/assets/current-conditions/wind.svg"
+                            current={19.3}
+                            currentIcon="/assets/current-conditions/wind.svg"
+                            unit="km/h"/>
+                    </div>
+
+                    <div className={`current-conditions-items ${classes.root} ${classes.currentConditionsBorder}`}>
+                        <div className="current-conditions-items-title">
+                            <h2>
+                                Padavine
+                            </h2>
+                        </div>
+                        <LoaderOverlay loading={isLoadingWeatherData} width={widthAndHeight} height={widthAndHeight}>
+                            <PrecipitationGraph unit={'mm'} value={10.4} widthAndHeight={195}/>
+                        </LoaderOverlay>
+                        <MinMaxCurrentValues loading={isLoadingWeatherData}
+                            max={201.6}
+                            maxIcon="/assets/current-conditions/rain.svg"
+                            current={0.8}
+                            currentIcon="/assets/current-conditions/rain.svg"
+                            unit="mm"/>
+                    </div>
+
+                    <div className={`current-conditions-items ${classes.root} ${classes.currentConditionsBorder}`}>
+                        <div className="current-conditions-items-title">
+                            <h2>
+                                Tlak
+                            </h2>
+                        </div>
+                        <LoaderOverlay loading={isLoadingWeatherData} width={widthAndHeight} height={widthAndHeight}>
+                            <CirclePointerChart unit={'hPa'} minValue={1009} value={1012} maxValue={1019} widthAndHeight={195}/>
+                        </LoaderOverlay>
+                        <MinMaxCurrentValues loading={isLoadingWeatherData}
+                            max={1019.9}
+                            maxIcon="/assets/current-conditions/pressure-gauge-max.svg"
+                            min={1009.2}
+                            minIcon="/assets/current-conditions/pressure-gauge-min.svg"
+                            unit="hPa"/>
                     </div>
                 </Carousel>
             </Box>
